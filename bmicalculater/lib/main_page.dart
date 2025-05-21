6842b1bd9af285291383380a4ad69ec9b0a747bb
D.N.Kodithuwakku <140165465+DilshaniNK@users.noreply.github.com>
2025-05-22 00:24:03 +0530

    add height and weight enter sections

--- bmicalculater/lib/main_page.dart
+++ bmicalculater/lib/main_page.dart
@@ -14,30 +14,114 @@ class _MainPageState extends State<MainPage> {
       body: 
         SafeArea(
           child: Container(
-            color: Colors.red,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
+            color: Colors.white,
+            
               child: Column(
                 children: [
                   Row(
                     children: [
-                      Column(
-                        children: [
-                          Text("Male Icon"),
-                          Text("Male")
-                        ],
+                      Padding(
+                        padding: const EdgeInsets.all(8.0), // add padding around male icon
+                        child: Column(
+                          children: const[
+                            Icon(Icons.male, size: 150,),
+                            Text("Male")
+                          ],
+                        ),
                       ),
-                      Column(
-                        children: [
-                          Text("Female Icon"),
-                          Text("Female")
-                        ],
+                      const Spacer(), //add space between male icon and female icon
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: const [
+                             Icon(Icons.female, size: 150,),
+                             Text("Female")
+                          ],
+                        ),
                       ),
+                     
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            const Text("Height"),
+                            const Text(
+                              "176", 
+                              style: TextStyle(
+                                color: Color(0xFFFF8888), 
+                                fontSize: 50, 
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                FloatingActionButton(onPressed: null, 
+                                  child: Icon(
+                                    Icons.remove, 
+                                    size: 40,
+                                  ),
+                                ),
+                                const SizedBox(width: 10),
+                                FloatingActionButton(onPressed: null, 
+                                  child: Icon(
+                                    Icons.add, 
+                                    size: 40,
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                            
+                        ),
+
+                      ),
+                      const Spacer(),
+                      
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            const Text("Weight"),
+                            const Text(
+                              "76", 
+                              style: TextStyle(
+                                color: Color(0xFFFF8888), 
+                                fontSize: 50, 
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                FloatingActionButton(onPressed: null, 
+                                  child: Icon(
+                                    Icons.remove, 
+                                    size: 40,
+                                  ),
+                                ),
+                                const SizedBox(width: 10),
+                                FloatingActionButton(onPressed: null, 
+                                  child: Icon(
+                                    Icons.add, 
+                                    size: 40,
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                            
+                        ),
+
+                      )
+                      
                     ],
                   )
                 ],
               ),
-            ),
+           
           ),
         ),
     );
