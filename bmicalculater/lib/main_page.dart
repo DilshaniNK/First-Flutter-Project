a43655b50e71a04b12ac6c59ef184fb0901eb65b
D.N.Kodithuwakku <140165465+DilshaniNK@users.noreply.github.com>
2025-05-21 18:22:05 +0530

    my first flutter project

---
+++ bmicalculater/lib/main_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: 
+        SafeArea(
+          child: Container(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Column(
+                        children: [
+                          Text("Male Icon"),
+                          Text("Male")
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text("Female Icon"),
+                          Text("Female")
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+    );
+  }
+}
